7c8165eb993d2617c4e206966d31744af3bee834
yonghyunc <chyh9218@gmail.com>
2022-12-07 21:44:17 +0900

    Feat [B-001] App 등록

--- Back/Dec_PJT/settings.py
+++ Back/Dec_PJT/settings.py
@@ -31,6 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'foods',
+    'community',
+    'accounts',
+    'rest_framework',
+    # 'corsheaders',
+    # 'rest_framework.authtoken',
+    # 'dj_rest_auth',
+    # 'django.contrib.sites',
+    # 'allauth',
+    # 'allauth.account',
+    # 'allauth.socialaccount',
+    # 'dj_rest_auth.registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,15 +51,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+# SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8080',
+# ]
 
 ROOT_URLCONF = 'Dec_PJT.urls'
 
