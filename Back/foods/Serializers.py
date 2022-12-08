629b8822606ec0441a5476a0cf9448639707cbc6
yonghyunc <chyh9218@gmail.com>
2022-12-08 22:00:18 +0900

    Feat [B-005] foods serializers 작성

---
+++ Back/foods/Serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from .models import Menu, Recipe, Ingredient
+
+
+
+# 재료
+class IngredientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+
+
+# 단일 레시피
+class RecipeSerializer(serializers.ModelSerializer):
+    ingredient = IngredientSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+
+
+# 단일 메뉴
+class MenuSerializer(serializers.ModelSerializer):
+    recipe = RecipeSerializer(read_only=True)
+
+    class Meta:
+        model = Menu
+        fields = '__all__'
+
+
+
+# 메뉴 리스트 
+class MenuListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Menu
+        fields = ('name', 'image',)
+
+
+
+# 레시피 리스트 (레시피 페이지, 재료 검색 페이지)
+class RecipeListSerializer(serializers.ModelSerializer):
+    ingredient = IngredientSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Recipe
+        fields = ('name', 'ingredient')
